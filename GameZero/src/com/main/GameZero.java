a6a6ec5a33d89acbae167d71c5b1e51fe2586c29
John Wilkins <jwilkinsspss@yahoo.com>
2016-10-04 13:35:12 -0500

    Added time variable to main class. Added attributes to Player class.

--- GameZero/src/com/main/GameZero.java
+++ GameZero/src/com/main/GameZero.java
@@ -2,6 +2,7 @@ package com.main;
 
 public class GameZero
 {
+	public long time;
 	public static void main(String[] args)
 	{
 		
--- GameZero/src/com/main/mechanics/Player.java
+++ GameZero/src/com/main/mechanics/Player.java
@@ -2,5 +2,15 @@ package com.main.mechanics;
 
 public class Player implements Entity
 {
-
+	public int health;
+	public int speed;
+	public int damage;
+	public int rangedDamage;
+	
+	public Player(int playerHealth, int playerSpeed, int playerDamage, int playerDamageRanged) {
+		health = playerHealth;
+		speed = playerSpeed;
+		damage = playerDamage;
+		rangedDamage = playerDamageRanged;
+	}
 }
