b4b09a9d8bfc81630a693b834e68edd79ee66dc7
John Wilkins <jwilkinsspss@yahoo.com>
2016-10-18 12:49:56 -0500

    Add basis for Start Menu

--- GameZero/src/com/main/GameZero.java
+++ GameZero/src/com/main/GameZero.java
@@ -1,12 +1,23 @@
 package com.main;
 
+import java.awt.Window;
+
+import javax.swing.JFrame;
+
 public class GameZero
 {
-	//Emily Test
 	public long time;
-	
+	private int width;
+	private int height;
+
 	public static void main(String[] args)
 	{
-		
+		JFrame startMenu = new JFrame();
+		startMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		startMenu.setSize(600, 400);
+		startMenu.setResizable(false);
+		startMenu.setLocationRelativeTo(null);
+		startMenu.setUndecorated(true);
+		startMenu.setVisible(true);
 	}
 }
