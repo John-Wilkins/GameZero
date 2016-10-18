7ccf80465b3bbe5012095a8a3111eb64d61a3d9e
John Wilkins <jwilkinsspss@yahoo.com>
2016-10-18 13:01:06 -0500

    Added Input Listener

---
+++ GameZero/src/com/main/controls/InputListener.java
@@ -0,0 +1,67 @@
+package com.main.controls;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class InputListener implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener
+{
+	public void keyPressed(KeyEvent event)
+	{
+		System.out.println("Key Pressed");
+	}
+
+	public void keyReleased(KeyEvent event)
+	{
+		System.out.println("Key Released");
+	}
+
+	public void keyTyped(KeyEvent event)
+	{
+		System.out.println("Key Typed");
+	}
+	
+	public void mouseClicked(MouseEvent event)
+	{
+		System.out.println("Mouse Clicked");
+	}
+
+	public void mouseEntered(MouseEvent event)
+	{
+		System.out.println("Mouse Entered");
+	}
+
+	public void mouseExited(MouseEvent event)
+	{
+		System.out.println("Mouse Exited");
+	}
+
+	public void mousePressed(MouseEvent event)
+	{
+		System.out.println("Mouse Pressed");
+	}
+
+	public void mouseReleased(MouseEvent event)
+	{
+		System.out.println("Mouse Released");
+	}
+
+	public void mouseDragged(MouseEvent arg0)
+	{
+		System.out.println("Mouse Dragged");
+	}
+
+	public void mouseMoved(MouseEvent arg0)
+	{
+		System.out.println("Mouse Moved");
+	}
+
+	public void mouseWheelMoved(MouseWheelEvent arg0)
+	{
+		System.out.println("Mouse Wheel Moved");
+	}
+}
